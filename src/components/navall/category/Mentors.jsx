fea149831330d88a15a08bdf3d8d92fb1d6b1427
ARNABDHUA <arnabdhua74@gmail.com>
2024-08-12 17:48:21 +0530

    update

--- src/components/navall/category/Mentors.jsx
+++ src/components/navall/category/Mentors.jsx
@@ -13,16 +13,17 @@ const Mentors = () => {
                    <div className=' container mt-2 mx-3 ' > 
                    <div className=' overflow-hidden rounded-3xl min-h-[200px] sm:min-h-[250px] hero-ag-color flex justify-center items-center '>
             <div className=' container pb-8 sm:pb-0 relative'>
-                <div className=' flex items-center gap-3 '>
-                 <img src={items.image} alt={items.name} className='max-h-[150px] sm:max-h-[270px] w-40 sm:w-72 rounded-xl sm:ml-4 ml-2' />
-                 <h3 className=' mb-2 text-xl sm:text-3xl relative bottom-6 sm:bottom-16 text-nowrap'>{items.name}</h3>
-                 <p className='mb-12 p-4 text-sm sm:text-lg relative top-12 sm:top-3 right-32 '>{items.title}</p>
+
+
+                <div className=' flex flex-col sm:flex-row items-center gap-1 sm:gap-3 '>
+                 <img src={items.image} alt={items.name} className='max-h-[150px] sm:max-h-[270px] w-40 sm:w-72 rounded-xl sm:ml-4 ml-2 mt-2 ' />
+                 <h3 className=' mb-2 text-xl sm:text-3xl  text-nowrap'>{items.name}</h3>
+                 <p className='mb-2 p-3 text-sm sm:text-lg text-nowrap '>{items.title}</p>
                  <p className='mb-12 p-4 text-sm sm:text-lg lg:block hidden'>{items.description}</p>
-                 <button  className='lg:block md:block mr-16 hidden relative right-5 bg-primary text-white bg-orange-500 cursor-pointer hover:scale-105 duration-300 py-2 px-8  rounded-full '>Message</button>
-                 
-                 </div>
+                 <button  className='lg:block  mr-16 hidden relative right-5 bg-primary text-white bg-orange-500 cursor-pointer hover:scale-105 duration-300 py-2 px-8  rounded-full '>Message</button>
                  <div className=' flex justify-center'>
-                 <button  className=' lg:hidden md:hidden mt-4 bg-primary text-white bg-orange-500 cursor-pointer hover:scale-105 duration-300 py-2 px-8 mx-2 rounded-full '>Message</button>
+                 <button  className=' lg:hidden  mt-4  text-white bg-orange-500 cursor-pointer hover:scale-105 duration-300 py-2 px-8 mx-2 rounded-full relative right-0 sm:right-40 top-0 sm:top-16 '>Message</button>
+                 </div>
                  
                  </div>
                  </div>
